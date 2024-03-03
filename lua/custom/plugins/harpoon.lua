d1783a84f98f0acd14521bd6b8a15265e74191a0
Xavier Fortin <xavier4-10@hotmail.com>
2024-03-03 18:52:36 -0500

    init

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,11 @@
+return {
+  'ThePrimeagen/harpoon',
+  config = function()
+    require('harpoon').setup {}
+    local mark = require 'harpoon.mark'
+    local ui = require 'harpoon.ui'
+
+    vim.keymap.set('n', '<leader>a', mark.add_file, { desc = 'Add file to harpoon' })
+    vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
+  end,
+}
